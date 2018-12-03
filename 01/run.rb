44fc945fcd90ea2e843822b6bda181b97d9924ac
shadish <shadtisch@gmail.com>
2018-12-02 21:34:19 -0700

    wip

--- 01/run.rb
+++ 01/run.rb
@@ -8,21 +8,16 @@ input = input.map{|i| i.to_i }
 # Part 2
 results = []
 pos = 0
-input.each do |i|
-	pos = pos + i
-	if results.include? pos
-		puts pos
-		# exit 0
+
+found = false
+while !found do
+	input.each do |i|
+		pos = pos + i
+		if results.include? pos
+			puts pos
+			found = true
+			exit 0
+		end
+		results.push pos
 	end
-	results.push pos
 end
-
-# res = Hash::new
-# results.each do |k|
-# 	if !res.key?(k)
-# 		res[k] = 0
-# 	end
-# 	res[k] += 1
-# end
-
-# puts res.select{|k,v| v > 1}
