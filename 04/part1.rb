1811880c96c76a5c0408e04df82184ecb49882ee
shadish <shadtisch@gmail.com>
2018-12-13 21:27:12 -0700

    finish day 4 part1

--- 04/part1.rb
+++ 04/part1.rb
@@ -3,21 +3,36 @@ require 'date'
 def parse(line)
 	d = line.scan(/\d+/).map{|d| d.to_i}
 	c = line.split(']')[1].strip!
-
 	{
 		:date => DateTime.new(d[0],d[1],d[2],d[3],d[4]),
 		:cmd => c.scan(/\d+/)[0] || c
 	}
 end
 
-# input = File.read('input.txt').split("\n")
-input = File.read('sample.txt').split("\n")
+input = File.read('input.txt').split("\n")
 	.map{|line| parse(line)}
 	.sort{|a,b| a[:date] <=> b[:date]}
 
-days = input
-	.map{|d| d[:date].strftime '%y-%m-%d' }
-	.uniq
+guards = input.map{|d| d[:cmd].to_i }.reject{|d| d == 0}.uniq
 	.inject({}) {|k,v| k[v] = Array.new(60,0); k }
 
-puts days
+
+current_guard = input[0][:cmd].to_i
+
+input.each_with_index do |v,i|
+	if(input[i][:cmd] == 'falls asleep')
+		(v[:date].minute..input[i+1][:date].minute-1).each do |m|
+			guards[current_guard][m] += 1
+		end
+	elsif(input[i][:cmd].to_i > 0)
+		current_guard = input[i][:cmd].to_i
+	end
+end
+
+results = guards.map{ |k,v|
+	max = v.max
+	sum = v.reduce{|a,b| a+b}
+	{:id => k, :sum => sum, :minute => v.find_index(max)}
+}.sort {|a,b| a[:sum] <=> b[:sum] }.reverse
+
+puts results.first[:id] * results.first[:minute]
